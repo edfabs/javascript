74f69d224f66fc2d1fe457d31b61b9ac55d13358
Fabian Suchett <suchettr@gmail.com>
2023-07-19 22:15:56 -0600

    add the creation of a array

--- sumOfARange/sumOfARange.js
+++ sumOfARange/sumOfARange.js
@@ -1,10 +1,13 @@
 function range(init, end){
     var suma = 0;
+    var arr= [];
     for(var i = init; i <= end; i++){
+        arr.push(i);
         suma = suma + i;
     }
+    console.log(arr)
     return suma
 }
 
-var sumRange = range(0, 4);
+var sumRange = range(8, 9);
 console.log(sumRange)
