90f4a8e202f98ea88cc1552ef709c51dac3adbce
Edgar Fabian Suchett Altamirano <suchettr@gmail.com>
2023-10-20 10:17:46 -0600

    Excercice of set is do it

--- classes/set/set.js
+++ classes/set/set.js
@@ -1,20 +1,33 @@
-const elementos = new Set();
-let john = { name: "John" };
-let pete = { name: "Pete" };
-let mary = { name: "Mary" };
-let fabian = {name: "Fabian"};
-let vector1 = {x: 3, y: 2};
-// elementos.add([3,2]);
-elementos.add(fabian);
-elementos.add(fabian);
-// elementos.add([3,2]);
-elementos.add(john);
-elementos.add(pete);
-elementos.add(mary);
-elementos.add(john);
-elementos.add(mary);
-elementos.add(vector1);
-elementos.add(vector1);
-elementos.delete(vector1);
-console.log(elementos.has("Mary"));
+class Conjunto{
+    constructor (){
+        this.elementos = [];
+    }
+    add(elemento){
+        if(this.elementos.indexOf(elemento) == -1){
+            this.elementos.push(elemento);
+            return console.log('El elemento se agrego correctamente');
+        }else{
+            return console.log('Elemento existente');
+        }
+    }
+    delete(elemento){
+        let index = this.elementos.indexOf(elemento);
+        console.log(`index :${index}`);
+        if(index != -1){
+            this.elementos.splice(index,1);
+        }
+        return this.elementos;
+    }
+    has(elemento){
+        if(this.elementos.indexOf(elemento) != -1){
+            return true;
+        }else return false;
+    }
+}
+var elementos = new Conjunto();
+console.log(elementos.add(2));
+console.log(elementos.add(3));
+console.log(elementos.add(3));
 console.log(elementos);
+console.log(elementos.has(4));
+console.log(elementos.delete(2));
