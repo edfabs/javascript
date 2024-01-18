1e76028c2a8d00da951bbe1092186aee07a623a5
Fabian Suchett <suchettr@gmail.com>
2024-01-17 22:01:05 -0600

    add file to track mapa.js

---
+++ classes/mapa/mapa.js
@@ -0,0 +1,10 @@
+/* function hasOwnProperty(valor){
+    if(valor)console.log(`true`);
+    else{
+        console.log(`false`);
+    }
+} */
+let mapa = {uno: true, doe: true};
+
+console.log(mapa.hasOwnProperty("uno"));
+/* hasOwnProperty.call(mapa); */
