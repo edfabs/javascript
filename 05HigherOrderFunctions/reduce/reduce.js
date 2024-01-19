11c314f0b305192f91368084dee1f40ffae0b67e
Fabian Suchett <suchettr@gmail.com>
2024-01-18 22:30:22 -0600

    Learning one more time the filter function

--- 05HigherOrderFunctions/reduce/reduce.js
+++ 05HigherOrderFunctions/reduce/reduce.js
@@ -1,12 +1,24 @@
 require("./load")("script.js");
 
-function cuentaDeCaracteres(codigo){
-    return codigo.ranges.reduce((cuenta, [desde, hasta]) => {
-        return cuenta + (hasta - desde);
-    }, 0);
-}
+// function cuentaDeCaracteres(codigo){
+//     return codigo.ranges.reduce((cuenta, [desde, hasta]) => {
+//         return cuenta + (hasta - desde);
+//     }, 0);
+// }
 
-console.log(SCRIPTS.reduce((a,b) => {
-    console.log(`a: ${a.ranges}, b: ${b.ranges}`);
-    return cuentaDeCaracteres(a) < cuentaDeCaracteres(b) ? b : a;
-}));
+// console.log(SCRIPTS.reduce((a,b) => {
+//     console.log(`a: ${a.ranges}, b: ${b.ranges}`);
+//     return cuentaDeCaracteres(a) < cuentaDeCaracteres(b) ? b : a;
+// }));
+
+// function filtrar (array, prueba){
+//     let pasaron = [];
+//     for (let elemento of array){
+//         if(prueba(elemento)){
+//             pasaron.push(elemento);
+//         }
+//     }
+//     return pasaron;
+// }
+// console.log(filtrar(SCRIPTS, codigo => codigo.living));
+console.log(SCRIPTS.filter(codigo => codigo.living == true));
