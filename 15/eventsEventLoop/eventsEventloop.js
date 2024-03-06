c787c4bd6ebc4c153ea1dad88a80f6252c9447ae
Edgar Fabian Suchett Altamirano <suchettr@gmail.com>
2024-03-06 11:58:02 -0600

    events loops files created to test this function but i will weithing for create a server to test like production this files

---
+++ 15/eventsEventLoop/eventsEventloop.js
@@ -0,0 +1,6 @@
+const squareWorker = new Worker("squareworker.js");
+squareWorker.addEventListener("message", event => {
+  console.log("The worker responded:", event.data);
+});
+squareWorker.postMessage(10);
+squareWorker.postMessage(24);
